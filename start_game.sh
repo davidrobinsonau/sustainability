54a8d0f5b170daf02b4425be15670d61674bce85
nwtas <115685597+nwtas@users.noreply.github.com>
2024-09-29 12:58:40 +1000

    Adding Event detection

--- start_game.sh
+++ start_game.sh
@@ -2,6 +2,7 @@
 
 # This script will start the Python script and restart it if it crashes
 # /home/hive/Workspace/sustainability/main.py
+cd /home/hive/Workspace/sustainability
 while true; do
     /home/hive/Workspace/sustainability/maingpiozero.py
 done

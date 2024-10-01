8c5336bb625e4378907ebd5863be1af57be9834d
nwtas <115685597+nwtas@users.noreply.github.com>
2024-10-01 14:40:05 +1000

    Adding Event detection

--- start_game.sh
+++ start_game.sh
@@ -4,7 +4,7 @@
 # /home/hive/Workspace/sustainability/main.py
 cd /home/hive/Workspace/sustainability
 while true; do
-    /home/hive/Workspace/sustainability/maingpiozero.py
+    /home/hive/Workspace/sustainability/maingpiozero.py >> /home/hive/Workspace/sustainability/log.txt 
     sleep 30
 done
 
